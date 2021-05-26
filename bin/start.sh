cb8e2a4684c00570d7264dffbd2183abefc3aab8
Diogo Dourado <dcdourado@gmail.com>
2021-05-25 22:57:52 -0300

    chore: improve logs of start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-echo "Executing qemu and gdb over main.s..."
+echo "Getting things started..."
 docker run \
     --name advanced_microprocessor \
     --mount source=programs_volume,target=/app \
     advanced_microprocessor:latest
 
+echo "Container up!"
+sleep 1
+
+echo "Executing qemu and gdb over main.s..."
 docker exec advanced_microprocessor  \
     qemu -g 10101 ./prg-tst-riscv1
 
+echo "riscv32-unknown-elf-gdb /app/main.s"
 docker exec -it advanced_microprocessor |
     riscv32-unknown-elf-gdb /app/main.s
